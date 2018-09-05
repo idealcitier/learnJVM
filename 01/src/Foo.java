dc014857823e66671b7e37ab69dd0baec52c9428
idealcitier <leeguo@126.com>
2018-09-05 22:19:03 +0800

    add 02

---
+++ 01/src/Foo.java
@@ -0,0 +1,11 @@
+public class Foo{
+	public static void main(String[] args){
+		boolean flag = true;
+		if(flag) System.out.println("hello java");
+
+		if(flag == true) System.out.println("hello JVM");
+
+	
+	}
+
+}
---
+++ 02/src/Foo.java
@@ -0,0 +1,14 @@
+public class Foo{
+	static boolean boolValue;
+	public static void main(String[] args){
+
+		
+		boolValue = 2;
+		if(boolValue) System.out.println("hello java");
+
+		if(boolValue == true) System.out.println("hello JVM");
+
+	
+	}
+
+}
